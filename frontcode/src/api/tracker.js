b86a8d11ec7dc0a7c10b17537f74b0e08ce782c3
revatidongare <revati@vinnovatetechnologies.com>
2020-10-09 22:14:16 +0530

    Mobile Authentication
    app both frontend and back end

---
+++ frontcode/src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: ' http://e91ee5b57ab7.ngrok.io',
+});
---
+++ frontcode/src/screens/SignupScreen.js
@@ -0,0 +1,40 @@
+import React, { useContext } from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Context as AuthContext } from '../context/AuthContext';
+import AuthForm from "../components/AuthForm";
+import NavLink from '../components/NavLink';
+
+const SignupScreen = ({ navigation }) => {
+  const { state, signup } = useContext(AuthContext);
+
+  return (
+    <View style={styles.container}>
+      <AuthForm
+      headerText= "Sign Up"
+      errorMessage={state.errorMessage}
+      submitButtonText=" Sign Up"
+      onSubmit={signup}
+      />      
+      <NavLink
+        routeName="Signin"
+        text="Already have an account? signin"
+      />
+    </View>
+  );
+};
+
+SignupScreen.navigationOptions = () => {
+  return {
+    header: () => false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250,
+  }
+});
+
+export default SignupScreen;
